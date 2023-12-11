c7f0c769be6f31b8fd376049a97648e9d17df813
Knoxjess <jessy1234592@gmail.com>
2023-12-10 21:24:30 -0600

    Nav Bar Comp

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner.js";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      
     </div>
   );
 }
